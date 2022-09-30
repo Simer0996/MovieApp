06ebcf8abfb38cf1e7c606a63dbe0fde8836578b
Simer <singhsimer09@gmail.com>
2022-09-30 12:59:30 -0700

    adding some changes

--- components/Menu.js
+++ components/Menu.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Select, Box, CheckIcon, Center } from "native-base";
 
 export default function Menu() {
-    return (
-        <View>
-            <Text>Menu</Text>
-        </View>
-    )
-}
+    const [service, setService] = React.useState("");
+    return <Center>
+        <Box maxW="300">
+            <Select selectedValue={service} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
+                bg: "teal.600",
+                endIcon: <CheckIcon size="5" />
+            }} mt={1} onValueChange={itemValue => setService(itemValue)}>
+                <Select.Item label="UX Research" value="ux" />
+                <Select.Item label="Web Development" value="web" />
+                <Select.Item label="Cross Platform Development" value="cross" />
+                <Select.Item label="UI Designing" value="ui" />
+                <Select.Item label="Backend Development" value="backend" />
+            </Select>
+        </Box>
+    </Center>;
+};
--- layout/Header.js
+++ layout/Header.js
@@ -1,15 +1,14 @@
-// import { View, Text } from 'react-native'
 import React from 'react'
-import {Box, StatusBar, HStack,Text} from 'native-base'
+import { Box, StatusBar, HStack, Text } from 'native-base'
 
 export default function Header() {
   return (
     <>
-        <StatusBar backgroundColor="#2c3e50" barStyle="light-content" />
-        <Box safeAreaTop backgroundColor="#2c3e50"></Box>
-        <HStack bg="#2c3e50" px={1} py={3} alignItems='center' justifyContent='center'>
+      <StatusBar backgroundColor="#2c3e50" barStyle="light-content" />
+      <Box safeAreaTop backgroundColor="#2c3e50"></Box>
+      <HStack bg="#2c3e50" px={1} py={3} alignItems='center' justifyContent='center'>
         <Text color='white' fontSize={20} fontWeight="bold">Movie App</Text>
-        </HStack>
+      </HStack>
     </>
   )
 }
