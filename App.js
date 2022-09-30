cb9ef33d90e0d8aa6b03fe58c570a61cac864610
Simer <singhsimer09@gmail.com>
2022-09-29 18:52:51 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
+import AppStack from './components/stacks/AppStack'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+    {/* <Header /> */}
+    {/* <RecipesListContainer /> */}
+    <AppStack />
+    <StatusBar style='auto' />
+  </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
